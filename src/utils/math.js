c639715996287d19d2b88f9dba0557240ec5190e
skynightdev <adrien.castel@ecole-isitech.fr>
2024-03-11 17:14:42 +0100

    Ajout des exos function & tableau terminé + fichiers dependant

--- src/utils/math.js
+++ src/utils/math.js
@@ -1,8 +1,10 @@
-export function divide (a, b) {
+export function divide(a, b) {
     if (b===0) {
         return "attempted to divide by 0"
     }
     return a / b;
 }
 
-exports.divide = divide;
+export const add = (a, b) => a + b;
+export const subtract = (a, b) => a - b;
+export const multiply = (a, b) => a * b;
